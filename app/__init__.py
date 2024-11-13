3094d2bf0bf3f198c6faadd5a2b4a611d67e6b71
arshthesingh <arshdeep.singh1118@outlook.com>
2024-11-13 15:06:40 -0500

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI']
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+
+    from app.routes import api
+    app.register_blueprint(api)
+
+    if not os.path.exists('site.db'):
+        with app.app_context():
+            db.create_all()
+
+    return app
