9f77f9b3a6b66d7bc34913f48ad2386433615172
arshthesingh <arshdeep.singh1118@outlook.com>
2024-12-03 15:43:18 -0500

    Adding tests folders which will contain various tests.

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from app import create_app, db
+
+@pytest.fixture
+def app():
+    """
+    Set up the Flask application for testing.
+    """
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:", # uses in memory DB for tests
+        "SECRET_KEY": "test_secret_key"
+    })
+
+    with app.app_context():
+        db.create_all()  
+        yield app
+        db.session.remove()
+        db.drop_all()  
+
+@pytest.fixture
+def client(app):
+    """
+    Provides a test client for the Flask application.
+    """
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    """
+    Provides a test CLI runner.
+    """
+    return app.test_cli_runner()
