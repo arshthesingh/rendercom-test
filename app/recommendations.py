7b1084beab670f905a49d6e540293f6b8b0fa448
arshthesingh <arshdeep.singh1118@outlook.com>
2025-01-11 18:36:17 -0500

    Updated recommendations.py to account for renaming function.

--- app/recommendations.py
+++ app/recommendations.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
-from app.movie_recommendation import get_recommendations
+from app.movie_recommendation import get_movie_recommendations
 
 recommendations_blueprint = Blueprint("recommendations", __name__)
 
 
 @recommendations_blueprint.route("/recommendations", methods=["GET"])
-@jwt_required()  # Protect route with JWT authentication
+@jwt_required()
 def get_recommendations():
     title = request.args.get("title")
 
     if not title:
         return jsonify({"error": "Movie title is required"}), 400
 
-    recommendations = get_recommendations(title)
+    recommendations = get_movie_recommendations(title)
 
     if not recommendations:
         return jsonify({"error": f"No recommendations found for {title}"}), 404
